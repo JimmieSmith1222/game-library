1d3b34ca0f732b58f02fcade82c038098445c7dd
SkyActually <jimmiesmith1222@gmail.com>
2024-06-26 16:33:01 -0500

    Commit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+require("dotenv").config()
+const port = process.env.PORT || 3000;
 const methodOverride = require('method-override');
 const gameRoutes = require('./routes/gameRoutes');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/game-library');
+// mongoose.connect('mongodb://localhost:27017/game-library');
+const mongoURI = process.env.MONGOURI;
 
 app.set('view engine', 'ejs');
 
@@ -13,6 +15,17 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use('/games', gameRoutes);
+
+async function connectToMongo() {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('The connection with MongoDB is established');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+
+connectToMongo();
 
 app.listen(port, () => {
   console.log('Server started on port:', port);
